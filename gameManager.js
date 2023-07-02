b1349693d80558b0ebdf7254925fce83590bbcba
Dimon Cheek <dimon.sergiienko@gmail.com>
2023-07-02 20:36:55 +0300

    add spawnEnemies.js, create spawnEnemies func

--- gameManager.js
+++ gameManager.js
@@ -1,6 +1,8 @@
 import { gameLoop } from "./gameLoop.js"
+import { spawnEnemies } from "./spawnEnemies.js"
 
 export function startGame() {
 console.log("gameManager: Start Game")
 gameLoop()
+spawnEnemies()
 }
---
+++ spawnEnemies.js
@@ -0,0 +1,6 @@
+export function spawnEnemies() {
+  const enemiesNum = 10
+  for (let i = 0; i < enemiesNum; i++) {
+    console.log("Spawning Enemy #", i)
+  }
+}
