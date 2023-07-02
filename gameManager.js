d7f0fcf425e45581d81abffcc9d3c6b5e0ac77a9
Dimon Cheek <dimon.sergiienko@gmail.com>
2023-07-02 20:51:12 +0300

    add gameState.js, createEnemy func, enemies array

--- gameManager.js
+++ gameManager.js
@@ -1,8 +1,11 @@
 import { gameLoop } from "./gameLoop.js"
 import { spawnEnemies } from "./spawnEnemies.js"
+import { gameState } from "./gameState.js"
 
 export function startGame() {
-console.log("gameManager: Start Game")
-gameLoop()
-spawnEnemies()
+  console.log("gameManager: Start Game")
+  gameLoop()
+  console.log(gameState)
+  spawnEnemies()
+  console.log(gameState)
 }
--- spawnEnemies.js
+++ spawnEnemies.js
@@ -1,6 +1,14 @@
+import { gameState } from "./gameState.js"
+
 export function spawnEnemies() {
   const enemiesNum = 10
   for (let i = 0; i < enemiesNum; i++) {
+    const enemy = createEnemy()
     console.log("Spawning Enemy #", i)
+    gameState.enemies.push(enemy)
   }
+}
+
+function createEnemy() {
+  return {x: 0, y: 0, vx: 10, vy: 5}
 }
