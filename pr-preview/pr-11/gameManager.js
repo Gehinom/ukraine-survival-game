feb328edbb7aaabad108f6d9e45ecf17c7d5d4e9
DarthVanger <DarthVanger@users.noreply.github.com>
2023-07-02 21:22:16 +0000

    Deploy preview for PR 11 🛫

--- pr-preview/pr-11/gameManager.js
+++ pr-preview/pr-11/gameManager.js
@@ -3,11 +3,15 @@ import { spawnEnemies } from "./spawnEnemies.js"
 import { gameState } from "./gameState.js"
 import { setCanvasSize } from "./canvas.js"
 import { spawnPlayer } from "./player.js"
+import { initKeyboard } from "./keyboard.js"
 
 export function startGame() {
   console.log("gameManager: Start Game")
 
   console.log("gameManeger, gameState: ", gameState)
+
+  console.log("gameManeger: init keyboard (wasd)")
+  initKeyboard()
 
   console.log("gameManager: Spawn Player")
   spawnPlayer()
