60becff0fb5352a9131632271612e032d2969253
Dimon Cheek <dimon.sergiienko@gmail.com>
2023-07-02 21:45:43 +0300

    add draw and update to the enemy

--- enemy.js
+++ enemy.js
@@ -1,7 +1,11 @@
+import { ctx } from "./canvas.js"
+
 export function createEnemy() {
   return {
     x: 0,
     y: 0,
+    height: 50,
+    width: 50,
     vx: 10,
     vy: 5,
     draw: drawEnemy,
@@ -11,8 +15,12 @@ export function createEnemy() {
 
 function drawEnemy() {
   console.log('drawEnemy')
+  console.log(this)
+  ctx.fillRect(this.x, this.y, this.height, this.width)
 }
 
 function updateEnemy() {
+  this.x += this.vx
+  this.y += this.vy
   console.log('updateEnemy') 
 }
