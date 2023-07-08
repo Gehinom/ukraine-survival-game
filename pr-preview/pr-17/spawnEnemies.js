0554c576c8bfa39c55b5cf777246609d7d68efe5
DarthVanger <DarthVanger@users.noreply.github.com>
2023-07-08 16:18:47 +0000

    Deploy preview for PR 17 🛫

---
+++ pr-preview/pr-17/spawnEnemies.js
@@ -0,0 +1,29 @@
+import { gameState } from "./gameState.js"
+import { createEnemy, enemySize, createTank } from "./enemy.js"
+
+export function spawnEnemies() {
+  const enemiesNum = 10
+  spawnInfantryWaveNorth(enemiesNum)
+  spawnTankWaveWest(enemiesNum)
+}
+
+function spawnInfantryWaveNorth(enemiesNum) {
+  for (let i = 0; i < enemiesNum; i++) {
+    const enemy = createEnemy({
+      x: i * 100,
+      y: -enemySize.height,
+    })
+    gameState.enemies.push(enemy)
+  }
+}
+
+function spawnTankWaveWest(enemiesNum) {
+  for (let i = 0; i < enemiesNum; i++) {
+    const enemy = createTank({
+      x: -enemySize.width,
+      y: i * (enemySize.height + 50),
+    })
+    gameState.enemies.push(enemy)
+  }
+}
+
