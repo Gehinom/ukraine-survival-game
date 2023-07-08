f18598b33d95946fd3a2f4d1bdc93652abd41442
Gehinom <111573690+Gehinom@users.noreply.github.com>
2023-07-08 12:58:11 +0000

    Deploying to gh-pages from @ Gehinom/ukraine-survival-game@c34f83759f4733c5dfadcc25be23085f5be73329 🚀

---
+++ player.js
@@ -0,0 +1,65 @@
+import { ctx } from "./canvas.js"
+import { gameState } from "./gameState.js"
+
+const playerWidth = 50;
+const playerHeight = playerWidth * 1080 / 640; // 1.6875
+
+export function spawnPlayer() {
+  gameState.player = {
+    img: createImage(),
+    x: window.innerWidth / 2,
+    y: window.innerHeight / 2,
+    height: playerHeight,
+    width: playerWidth,
+    vx: 0,
+    vy: 0,
+    hp: 100,
+    draw: drawPlayer,
+    update: updatePlayer,
+    controlsState: {
+      isMovingLeft: false,
+      isMovingRight: false,
+      isMovingDown: false,
+      isMovingUp: false,
+    },
+    speed: 2.5,
+  }
+}
+
+function createImage() {
+  const img = new Image()
+  img.src = "playerImg/saint-javelin-skin-1.webp"
+  return img
+}
+
+function drawPlayer() {
+  ctx.drawImage(this.img, this.x, this.y, this.width, this.height)
+}
+
+function updatePlayer() {
+  this.x += this.vx
+  this.y += this.vy
+
+  if (this.controlsState.isMovingUp) {
+    this.vy = -this.speed
+  }
+  if (this.controlsState.isMovingDown) {
+    this.vy = this.speed
+  }
+  if (this.controlsState.isMovingLeft) {
+    this.vx = -this.speed
+  }
+  if (this.controlsState.isMovingRight) {
+    this.vx = this.speed
+  }
+
+  if ( !this.controlsState.isMovingRight && !this.controlsState.isMovingLeft
+  ) {
+    this.vx = 0
+  }
+
+  if ( !this.controlsState.isMovingUp && !this.controlsState.isMovingDown) {
+    this.vy = 0
+  }
+
+}
