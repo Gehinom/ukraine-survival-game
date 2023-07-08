ac3eef500a03fd7d2015eea0830425e1bfe80db6
hellgamer <19thanks96@gmail.com>
2023-07-08 17:34:40 +0300

    add spawnArtilleryWaveWest

--- spawnEnemies.js
+++ spawnEnemies.js
@@ -4,6 +4,7 @@ import { createEnemy, enemySize } from "./enemy.js"
 export function spawnEnemies() {
   const enemiesNum = 10
   spawnInfantryWaveNorth(enemiesNum)
+  spawnArtilleryWaveWest(enemiesNum)
 }
 
 function spawnInfantryWaveNorth(enemiesNum) {
@@ -17,3 +18,16 @@ function spawnInfantryWaveNorth(enemiesNum) {
     gameState.enemies.push(enemy)
   }
 }
+
+function spawnArtilleryWaveWest(enemiesNum) {
+  for (let i = 0; i < enemiesNum; i++) {
+    const enemy = createEnemy({
+      x: -enemySize.width,
+      y: i * (enemySize.height + 50),
+      vx: 0,
+      vy: 1,
+    })
+    gameState.enemies.push(enemy)
+  }
+}
+
